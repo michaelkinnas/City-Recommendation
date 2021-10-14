bd8ab74de5f02264f8f8328e203e89044cafbe79
Michael Kinnas <mkinnas86@gmail.com>
2021-10-14 11:27:42 +0300

    adde functionallity

--- CityRecommend/src/cityrecommend/CityRecommend.java
+++ CityRecommend/src/cityrecommend/CityRecommend.java
@@ -15,7 +15,45 @@ public class CityRecommend {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double dist = distance(37.983810, 23.727539, -33.868820, 151.209290, 'K');        
+        
+        double[] testFeatures = {4,5,1,5,6,3,8, 260, 65, dist};
+        
+        City testing = new City(testFeatures);
+        testing.testPrint();
+        
+        System.out.println(dist);
     }
+    
+    
+    //WILL PROBABLY BE IMPLEMENTED IN A CLASS
+    private static double distance(double lat1, double lon1, double lat2, double lon2, char unit) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+         if (unit == 'K') {
+            dist = dist * 1.609344;
+        } else if (unit == 'N') {
+            dist = dist * 0.8684;
+        }
+        return (dist);
+    }
+        
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts decimal degrees to radians             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private static double deg2rad(double deg) {
+      return (deg * Math.PI / 180.0);
+    }
+    
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts radians to decimal degrees             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private static double rad2deg(double rad) {
+      return (rad * 180.0 / Math.PI);
+    }
+    
     
 }
---
+++ CityRecommend/src/cityrecommend/PerceptronYoungTraveller.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cityrecommend;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Michail Kinnas it22046
+ */
+public class PerceptronYoungTraveller implements PerceptronTraveller{
+    float weightBias = 0; //TO BE IMPEMENTED
+    
+    public ArrayList<City> recommend(){
+        return null; //TO BE IMPLEMENTED
+    };
+    
+}
