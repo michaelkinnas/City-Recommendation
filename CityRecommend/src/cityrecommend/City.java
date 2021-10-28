0a2882f892d896df9776031671c79c6598bab091
Michael Kinnas <mkinnas86@gmail.com>
2021-10-28 10:55:36 +0300

    refactoring

--- CityRecommend/src/cityrecommend/City.java
+++ CityRecommend/src/cityrecommend/City.java
@@ -40,35 +40,23 @@ public class City {
         private static final int FEATUREMIN = 0;
         private static final int TEMPMAX = 331;
         private static final int TEMPMIN = 184;
-
-        private static double[] populateData(String cityName, String countryInitials, String appid, String[] keywords) {            
-            double unormalizedFeatures[] = new double[10];
-            double featureArr[] = new double[7];
-            double weatherArr[] = new double[3]; 
-            
+        
+        private static final double unnormalizedFeatures[] = new double[10]; 
+        
+        
+        private static double[] populateData(String cityName, String countryInitials, String appid, String[] keywords) {
             try {
-                featureArr = retrieveFeatureCount(cityName, keywords);
+                retrieveFeatureCount(cityName, keywords);
+            } catch (IOException e) {
+                //DO SOMETHING? HOW TO THROW EXCEPTION?
+            }
+            try {
+                retrieveWeatherData(cityName, countryInitials, appid);
             } catch (IOException e) {
                 //DO SOMETHING? HOW TO THROW EXCEPTION?
             }           
-            
-            try {
-                weatherArr = retrieveWeatherData(cityName, countryInitials, appid);
-            } catch (IOException e) {
-                //DO SOMETHING? HOW TO THROW EXCEPTION?
-            }      
-            
-            for (int i = 0; i < 7; i++) {
-                unormalizedFeatures[i] = featureArr[i];                
-            }
-            
-            for (int i = 0; i < 3; i++) {
-                unormalizedFeatures[i+7] = weatherArr[i];
-            }
-                        
-            return normalizedFeatures(unormalizedFeatures);
-        }        
-
+            return normalizedFeatures(unnormalizedFeatures);
+        } 
         /*
         Calculates distance between two sets of longitude and latitude in signed degrees format (DDD.dddd)
         Using real numbers: East-West from -180.0000 to 180.0000 and North-South from 90.0000 to -90.0000
@@ -98,6 +86,7 @@ public class City {
         private static double rad2deg(double rad) {
           return (rad * 180.0 / Math.PI);
         }
+        
 
         private static double[] normalizedFeatures(double[] features){       
             double[] normalizedArray = new double[10];
@@ -111,62 +100,55 @@ public class City {
 
             return normalizedArray;
         }
+        
 
         private static double featureNormalizer(double feature, int max, int min) {
             return (feature - min) / (max - min);
         }
+        
 
         private static double geodesicNormalizer(double distance, int max) {        
             return distance / max;
         }
         
-        private static double[] retrieveWeatherData(String city, String country, String appid) throws  IOException {
-            double[] weatherArr = new double[3];
+        
+        private static void retrieveWeatherData(String city, String country, String appid) throws  IOException {            
             ObjectMapper mapper = new ObjectMapper();            
           
             OpenWeatherMap weather_obj = mapper.readValue(new URL("http://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&APPID="+appid+""), OpenWeatherMap.class);
             //System.out.println(city+" temperature: " + (weather_obj.getMain()).getTemp());
             //System.out.println(city+" lat: " + weather_obj.getCoord().getLat()+" lon: " + weather_obj.getCoord().getLon());
             
-            weatherArr[0] = weather_obj.getMain().getTemp();
-            weatherArr[1] = weather_obj.getClouds().getAll();
-            weatherArr[2] = calculateDistance(ATHENSLAT, ATHENSLON, weather_obj.getCoord().getLat(), weather_obj.getCoord().getLon(), 'K');
-            
-            return weatherArr;
+            unnormalizedFeatures[7] = weather_obj.getMain().getTemp();
+            unnormalizedFeatures[8] = weather_obj.getClouds().getAll();
+            unnormalizedFeatures[9] = calculateDistance(ATHENSLAT, ATHENSLON, weather_obj.getCoord().getLat(), weather_obj.getCoord().getLon(), 'K');            
         }
         
-        /** Creates an array with the counted keywords from the wiki page.
-        @param city The string with name of the city.
-        @param keywords The string array with the 7 keywords.
-        @return An array of size 7 with counted keywords.        
-        */
-        private static double[] retrieveFeatureCount(String city, String[] keywords) throws  IOException {            
+       
+        private static void retrieveFeatureCount(String city, String[] keywords) throws  IOException {            
             ObjectMapper mapper = new ObjectMapper();
             
             MediaWiki mediaWiki_obj = mapper.readValue(new URL("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles="+city+"&format=json&formatversion=2"),MediaWiki.class);
             //System.out.println(city+" Wikipedia article: "+mediaWiki_obj.getQuery().getPages().get(0).getExtract()); //DEBUG MESSAGE
                         
-            return countWords(mediaWiki_obj, keywords);
-        }       
-        
-        private static double[] countWords(MediaWiki mediaWiki_obj, String[] keywords) {
-            double[] wordsCount = new double[7];
-            
-            for (int i = 0; i < 7; i++) {
-                wordsCount[i] = countCriterionfCity(mediaWiki_obj.getQuery().getPages().get(0).getExtract(), keywords[i]);
-                if (wordsCount[i] > 10) {
-                    wordsCount[i] = 10;
-                }
-            }            
-            return wordsCount;
+            countWords(mediaWiki_obj, keywords);
         }
         
+        
+        private static void countWords(MediaWiki mediaWiki_obj, String[] keywords) {
+            for (int i = 0; i < 7; i++) {
+                unnormalizedFeatures[i] = countCriterionfCity(mediaWiki_obj.getQuery().getPages().get(0).getExtract(), keywords[i]);
+                if (unnormalizedFeatures[i] > 10) {
+                    unnormalizedFeatures[i] = 10;
+                }
+            }
+        } 
         /** Counts the number of times a criterion occurs in the city wikipedia article.
         @param cityArticle  The String of the retrieved wikipedia article.
         @param criterion The String of the criterion we are looking for.
         @return An integer, the number of times the criterion-string occurs in the wikipedia article.
         */	
-        public static int countCriterionfCity(String cityArticle, String criterion) {
+        private static int countCriterionfCity(String cityArticle, String criterion) {
             cityArticle=cityArticle.toLowerCase();
             int index = cityArticle.indexOf(criterion);
             int count = 0;
